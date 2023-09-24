b287a68b8682e059c5638ced36a5a3d92fbecaac
Yuanqing <yl11249@nyu.edu>
2023-09-24 16:23:28 -0400

    25 lines drawing

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ Image with url:
 
 
 Image on repo:  
-![image description](./file-name.jpg)
+![Process](./HW03C.jpg)
 
 
 To start a new line, add two spaces at the end of a line, like this:  
--- sketch.js
+++ sketch.js
@@ -7,14 +7,14 @@ function draw() {
     stroke('black');
     randomSeed(110);
 
-    for (let xPos1 = 0; xPos1 <= width; xPos1 += random(30, 50)) {
-        for (let yPos1 = 0; yPos1 <= height; yPos1 += random(30, 50)) {
+    for (let i = 0; i < 25; i += 1) {
+        let xPos1 = 0; xPos1 <= width; xPos1 += random(30, width - 30)
+        let yPos1 = 0; yPos1 <= height; yPos1 += random(30, height - 30)
 
-            // Select a random point for connecting
-            let xPos2 = random(0, width);
-            let yPos2 = random(0, height);
+        // Select a random point for connecting
+        let xPos2 = random(0, width);
+        let yPos2 = random(0, height);
 
-            line(xPos1, yPos1, xPos2, yPos2);
-        }
+        line(xPos1, yPos1, xPos2, yPos2);
     }
 }
